3af09f00bd84e7b5da791833a05825e761188c0b
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2023-06-06 14:36:22 +0200

    Add project files.

---
+++ Scala.Adovb4.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Scala.Adovb4.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LstMenus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void CmbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void BtnClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
